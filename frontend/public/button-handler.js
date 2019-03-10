5efd850f8f22bb85ea724c4caabe90228499e66d
jchen <janetchen906@gmail.com>
2019-03-09 18:29:26 -0800

    all the front-end stuff

---
+++ frontend/public/button-handler.js
@@ -0,0 +1,12 @@
+(function($) {
+// find elements
+    $("#banner-message").click((e)=> {
+        $(this).addClass("alt");
+    })
+//     var button = $("button");
+//
+// // handle click and add class
+//     button.on("click", function(){
+//         banner.addClass("alt")
+//     });
+})(jQuery);
